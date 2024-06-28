51f56dcbbfb1d9c0a65eac62009bab51d0a18c99
Spyware08 <0699gurpreet@gmail.com>
2024-06-28 21:28:29 +0530

    updates default user

--- Frontend/src/Components/User/Signin.jsx
+++ Frontend/src/Components/User/Signin.jsx
@@ -1,63 +1,65 @@
-import React, { useRef } from 'react'
-import { useNavigate } from "react-router-dom"
-import API from '../../API/API'
+import React, { useRef, useState } from 'react';
+import { useNavigate } from "react-router-dom";
+import API from '../../API/API';
 import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer, toast } from 'react-toastify';
 
-import "./signup.css"
-import "./signin.css"
+import "./signup.css";
+import "./signin.css";
 
 const Signin = () => {
-    const email = useRef()
-    const password = useRef()
-    const navigate = useNavigate()
+    const navigate = useNavigate();
+    const [email, setEmail] = useState('demouser@gmail.com'); 
+    const [password, setPassword] = useState('12345678'); 
 
     async function handleSubmit(e) {
-        e.preventDefault()
-        if (email.current.value.length>0 
-            && password.current.value.length>0) {
-
+        e.preventDefault();
+        if (email.length > 0 && password.length > 0) {
             let log_data = {
-                email: email.current.value,
-                password: password.current.value
-            }
+                email: email,
+                password: password
+            };
 
             try {
-                const res = await API.post("/login", log_data)
+                const res = await API.post("/login", log_data);
                 sessionStorage.setItem("userData", JSON.stringify(res.data.userdata));
-                navigate("/welcome")
+                navigate("/welcome");
             } catch (e) {
                 console.error(e.response.status);
-                if(e.response.status===404){
-                    return toast.info("Username not found")
+                if (e.response.status === 404) {
+                    toast.info("Username not found");
+                } else if (e.response.status === 401) {
+                    toast.info("Password not matched");
                 }
-                else if(e.response.status===401){
-                    return toast.info("Passward not matched")
-
-                }
-
             }
+        } else {
+            toast.error("Invalid Details");
         }
-        else return toast.error("Invalid Details")
     }
 
     return (
         <>
             <div className='signin_main'>
                 <div className='siginin_content'>
-                    <h1>Login  Here</h1>
+                    <h1>Login Here</h1>
                 </div>
                 <div className='signin_input'>
-                    <input type="text" placeholder='Username' ref={email} />
-                    <input type="password" placeholder='Passward' ref={password} />
-
+                    <input
+                        type="text"
+                        placeholder='Username'
+                        value={email} 
+                        onChange={(e) => setEmail(e.target.value)} 
+                    />
+                    <input
+                        type="password"
+                        placeholder='Password'
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
                 </div>
                 <div className='signin_btn'>
-                    <button className='signin_log' onClick={handleSubmit} >Login</button>
+                    <button className='signin_log' onClick={handleSubmit}>Login</button>
                 </div>
-                {/* <div className='signin_footer_p'>
-                    <p>Or Login with <span>Google</span></p>
-                </div> */}
             </div>
             <ToastContainer
                 position="top-right"
@@ -71,9 +73,8 @@ const Signin = () => {
                 pauseOnHover
                 theme="dark"
             />
-
         </>
-    )
+    );
 }
 
-export default Signin
+export default Signin;
